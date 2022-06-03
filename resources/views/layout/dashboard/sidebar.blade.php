5a542f822c2456ba97445a09e2dfbae9dac91371
tzelginianc <tzelginia78@gmail.com>
2022-06-03 19:49:03 +0700

    update sidebar

--- resources/views/layout/dashboard/sidebar.blade.php
+++ resources/views/layout/dashboard/sidebar.blade.php
@@ -1,19 +1,19 @@
 <nav class="sidebar sidebar-offcanvas" id="sidebar">
-    <ul class="nav">
-        <li class="nav-item nav-category">Main</li>
-        <li class="nav-item">
-            <a class="nav-link" href="index.html">
-                <span class="icon-bg"><i class="mdi mdi-cube menu-icon"></i></span>
-                <span class="menu-title">Dashboard</span>
-            </a>
-        </li>
-        <!-- <li class="nav-item">
+  <ul class="nav">
+    <li class="nav-item nav-category">Menu</li>
+    <li class="nav-item">
+      <a class="nav-link" href="index.html">
+        <span class="icon-bg"><i class="mdi mdi-cube menu-icon"></i></span>
+        <span class="menu-title">Dashboard</span>
+      </a>
+    </li>
+    <!-- <li class="nav-item">
         <a class="nav-link" data-bs-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
           <span class="icon-bg"><i class="mdi mdi-crosshairs-gps menu-icon"></i></span>
           <span class="menu-title">UI Elements</span>
           <i class="menu-arrow"></i>
         </a> -->
-        <!-- <div class="collapse" id="ui-basic">
+    <!-- <div class="collapse" id="ui-basic">
           <ul class="nav flex-column sub-menu">
             <li class="nav-item"> <a class="nav-link" href="pages/ui-features/buttons.html">Buttons</a></li>
             <li class="nav-item"> <a class="nav-link" href="pages/ui-features/dropdowns.html">Dropdowns</a></li>
@@ -21,12 +21,12 @@
           </ul>
         </div>
       </li> -->
-        <!-- <li class="nav-item">
+    <!-- <li class="nav-item">
       <a class="nav-link" href="pages/icons/mdi.html">
         <span class="icon-bg"><i class="mdi mdi-contacts menu-icon"></i></span>
         <span class="menu-title">Icons</span>
       </a>
     </li> -->
 
-    </ul>
+  </ul>
 </nav>
