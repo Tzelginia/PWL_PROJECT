2234043aae23d75bb8d05c387ed45e54089956aa
tzelginianc <tzelginia78@gmail.com>
2022-06-06 21:33:36 +0700

    Migration : Menambahkan kolom role dan verify

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['admin', 'pembeli'])->default('pembeli');
+            $table->integer('verify')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
