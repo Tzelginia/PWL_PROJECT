ce762e371ba498e0943a41005c2e33a561443f88
tzelginianc <tzelginia78@gmail.com>
2022-06-08 09:32:54 +0700

    Hapus Tabel

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -4,19 +4,7 @@
 
 <div class="table-responsive">
     <div class="d-xl-flex justify-content-between align-items-start">
-        <h3 class="text-dark font-weight-bold mb-3">Daftar Barang </h3>
+        <h2 class="text-dark font-weight-bold mb-3">Selamat Datang Di Pixie Furniture </h2>
     </div>
-    <table class="table table-head-fixed table-hover">
-        <thead>
-            <tr>
-                <th>Kode Produk</th>
-                <th>Nama Produk</th>
-                <th>Merk Produk</th>
-                <th>Kategori</th>
-                <th>Stok</th>
-                <th>Harga</th>
-            </tr>
-        </thead>
-    </table>
 </div>
 @endsection
