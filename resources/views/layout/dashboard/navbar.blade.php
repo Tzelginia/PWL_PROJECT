538f7e523a4522003295bfe3b0f8389109f3bf51
tzelginianc <tzelginia78@gmail.com>
2022-06-08 09:33:24 +0700

    Ubah Logo

--- resources/views/layout/dashboard/navbar.blade.php
+++ resources/views/layout/dashboard/navbar.blade.php
@@ -1,14 +1,14 @@
 <nav class="navbar default-layout-navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
     <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
-        <a class="navbar-brand brand-logo" href="/"><img src="{{asset('dashboard/images/logo.svg')}}" alt="logo" /></a>
-        <a class="navbar-brand brand-logo-mini" href="/"><img src="{{asset('dashboard/images/logo-mini.svg')}}" alt="logo" /></a>
+        <a class="navbar-brand brand-logo" href="/"><img src="{{asset('dashboard/images/dashboard/header-logo.png')}}" alt="Pixie Furniture" /></a>
+        <a class="navbar-brand brand-logo-mini" href="/"><img src="{{asset('dashboard/images/dashboard/header-logo.png')}}" alt="Pixie Furniture" /></a>
     </div>
     <div class="navbar-menu-wrapper d-flex align-items-stretch">
         <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
             <span class="mdi mdi-menu"></span>
         </button>
         <div class="search-field d-none d-xl-block">
-            <form class="d-flex align-items-center h-100" action="/search">
+            <form class="d-flex align-items-center h-100" aSction="/search">
                 <div class="input-group">
                     <div class="input-group-prepend bg-transparent">
                         <i class="input-group-text border-0 mdi mdi-magnify"></i>
