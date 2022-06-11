23b53dd82ae21722a4641bfcd68e42dfba05901d
tzelginianc <tzelginia78@gmail.com>
2022-06-12 00:12:24 +0700

    View Contact Pelanggan

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -26,26 +26,22 @@
               <div class="col-md-6">
                   <div class="right-content">
                       <div class="container">
-                          <form id="contact" action="" method="post">
+                          @if (session()->has('success'))
+                          <div class="alert alert-success col-lg-12 mt-3 mb-3" role="alert">
+                              {{ session('success') }}
+                          </div>
+                          @endif
+                          <form id="contact" action="/review" method="post">
+                              @csrf
                               <div class="row">
-                                  <div class="col-md-6">
+                                  <div class="col-md-12">
                                       <fieldset>
-                                          <input name="name" type="text" class="form-control" id="name" placeholder="Your name..." required="">
-                                      </fieldset>
-                                  </div>
-                                  <div class="col-md-6">
-                                      <fieldset>
-                                          <input name="email" type="text" class="form-control" id="email" placeholder="Your email..." required="">
+                                          <input name="subject" type="text" class="form-control" id="subject" placeholder="Subject...">
                                       </fieldset>
                                   </div>
                                   <div class="col-md-12">
                                       <fieldset>
-                                          <input name="subject" type="text" class="form-control" id="subject" placeholder="Subject..." required="">
-                                      </fieldset>
-                                  </div>
-                                  <div class="col-md-12">
-                                      <fieldset>
-                                          <textarea name="message" rows="6" class="form-control" id="message" placeholder="Your message..." required=""></textarea>
+                                          <textarea name="isi" rows="6" class="form-control" id="isi" placeholder="Your message..."></textarea>
                                       </fieldset>
                                   </div>
                                   <div class="col-md-12">
