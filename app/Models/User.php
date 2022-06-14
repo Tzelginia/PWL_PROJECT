3857aeb50546d677a1bd8e4c634903725ec97029
tzelginianc <tzelginia78@gmail.com>
2022-06-14 20:40:32 +0700

    Relasi dengan keranjang

--- app/Models/User.php
+++ app/Models/User.php
@@ -48,4 +48,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Review::class);
     }
+
+    public function keranjang()
+    {
+        return $this->hasMany(Keranjang::class);
+    }
 }
