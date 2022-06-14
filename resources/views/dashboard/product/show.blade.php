0fc57f76564b7639115f2997caa7e6fc1b1a79cd
tzelginianc <tzelginia78@gmail.com>
2022-06-14 20:45:52 +0700

    Menambahkan foto

--- resources/views/dashboard/product/show.blade.php
+++ resources/views/dashboard/product/show.blade.php
@@ -3,6 +3,7 @@
 <div class="container mt-5">
     <div class="row justify-content-center align-items-center">
         <div class="card" style="width: 30rem;">
+            <img src="{{ asset('storage/' . $product->file_pendukung) }}" class="card-img-top" alt="{{ $product->file_pendukung }}">
             <div class="card-header">
                 Detail Product
             </div>
@@ -10,8 +11,7 @@
                 <ul class="list-group list-group-flush">
                     <li class="list-group-item"><b>Kategori : </b>{{ $product->category->nama_category  }}</li>
                     <li class="list-group-item"><b>Nama : </b>{{ $product->nama }}</li>
-                    <li class="list-group-item"><b>Harga : </b>{{ $product->harga }}</li>
-                    <li class="list-group-item"><b>Gambar : </b>{{ $product->file_pendukung }}</li>
+                    <li class="list-group-item"><b>Harga : </b>Rp.{{ $product->harga }}</li>
             </div>
             <a class="btn btn-success mt-3 mb-3" href="/dashboard/product">Kembali</a>
         </div>
