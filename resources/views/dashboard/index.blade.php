11c0df3d7f19aa56f993c243f44c8c5914e82b8b
= <=>
2022-06-14 12:08:10 +0700

    product

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -2,9 +2,29 @@
 
 @section('content')
 
-<div class="table-responsive">
-    <div class="d-xl-flex justify-content-between align-items-start">
-        <h2 class="text-dark font-weight-bold mb-3">Selamat Datang Di Pixie Furniture </h2>
-    </div>
-</div>
+    <style>
+        h2
+        {
+            background-color: rgb(219, 223, 248);
+            color: rgb(46, 12, 170);
+            font-family: sans-serif;
+            text-align: center;
+            width: 45%;
+            margin:auto;
+            padding: 20px;
+        }
+ 
+        body
+        {
+            background-image: url("assets/images/biru.jpg");
+            background-repeat: ;
+            background-size: 80%;
+        }
+    </style>
+</head>
+<body>
+ 
+    <h2>Selamat Datang Di Pixie Furniture</h2>
+ 
+</body>
 @endsection
--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -29,81 +29,81 @@
         <div id="1" class="item new col-md-4">
             <a href="single-product.html">
                 <div class="featured-item">
-                    <img src="assets/images/product-01.jpg" alt="">
-                    <h4>Proin vel ligula</h4>
-                    <h6>$15.00</h6>
+                    <img src="assets/images/kursi.jpg" alt="">
+                    <h4>Kursi Santai</h4>
+                    <h6>Rp. 400.000</h6>
                 </div>
             </a>
         </div>
         <div id="2" class="item high col-md-4">
             <a href="single-product.html">
                 <div class="featured-item">
-                    <img src="assets/images/product-02.jpg" alt="">
-                    <h4>Erat odio rhoncus</h4>
-                    <h6>$25.00</h6>
+                    <img src="assets/images/kursi_2.jpg" alt="">
+                    <h4>Kursi Kayu</h4>
+                    <h6>Rp. 200.000</h6>
                 </div>
             </a>
         </div>
         <div id="3" class="item low col-md-4">
             <a href="single-product.html">
                 <div class="featured-item">
-                    <img src="assets/images/product-03.jpg" alt="">
-                    <h4>Integer vel turpis</h4>
-                    <h6>$35.00</h6>
+                    <img src="assets/images/kursi_3.jpg" alt="">
+                    <h4>Kursi Sofa</h4>
+                    <h6>Rp. 800.000</h6>
                 </div>
             </a>
         </div>
         <div id="4" class="item low col-md-4">
             <a href="single-product.html">
                 <div class="featured-item">
-                    <img src="assets/images/product-04.jpg" alt="">
-                    <h4>Sed purus quam</h4>
-                    <h6>$45.00</h6>
+                    <img src="assets/images/meja.jpg" alt="">
+                    <h4>Rak Meja</h4>
+                    <h6>Rp. 150.000</h6>
                 </div>
             </a>
         </div>
         <div id="5" class="item new high col-md-4">
             <a href="single-product.html">
                 <div class="featured-item">
-                    <img src="assets/images/product-05.jpg" alt="">
-                    <h4>Morbi aliquet</h4>
-                    <h6>$55.00</h6>
+                    <img src="assets/images/meja_2.jpg" alt="">
+                    <h4>Meja Kayu Jati</h4>
+                    <h6>Rp. 1.000.000</h6>
                 </div>
             </a>
         </div>
         <div id="6" class="item new col-md-4">
             <a href="single-product.html">
                 <div class="featured-item">
-                    <img src="assets/images/product-06.jpg" alt="">
-                    <h4>Urna ac diam</h4>
-                    <h6>$65.00</h6>
+                    <img src="assets/images/meja_3.jpg" alt="">
+                    <h4>Meja Biasa</h4>
+                    <h6>Rp. 100.000</h6>
                 </div>
             </a>
         </div>
         <div id="7" class="item new high col-md-4">
             <a href="single-product.html">
                 <div class="featured-item">
-                    <img src="assets/images/product-03.jpg" alt="">
-                    <h4>Proin eget imperdiet</h4>
-                    <h6>$75.00</h6>
+                    <img src="assets/images/almari.jpg" alt="">
+                    <h4>Almari 2 Pintu</h4>
+                    <h6>Rp. 500.000</h6>
                 </div>
             </a>
         </div>
         <div id="8" class="item low new col-md-4">
             <a href="single-product.html">
                 <div class="featured-item">
-                    <img src="assets/images/product-02.jpg" alt="">
-                    <h4>Nullam risus nisl</h4>
-                    <h6>$85.00</h6>
+                    <img src="assets/images/almari_2.jpg" alt="">
+                    <h4>Almmari Pintu Geser</h4>
+                    <h6>Rp. 600.000</h6>
                 </div>
             </a>
         </div>
         <div id="9" class="item new col-md-4">
             <a href="single-product.html">
                 <div class="featured-item">
-                    <img src="assets/images/product-01.jpg" alt="">
-                    <h4>Cras tempus</h4>
-                    <h6>$95.00</h6>
+                    <img src="assets/images/almari_3.jpg" alt="">
+                    <h4>Almari + Kaca</h4>
+                    <h6>Rp. 1.500.000</h6>
                 </div>
             </a>
         </div>
