a76884b60cb13ed7ec34f152fb3100f3dfb1133c
tzelginianc <tzelginia78@gmail.com>
2022-06-20 17:54:56 +0700

    View Product tampilan utama

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -1,50 +1,60 @@
 @extends('layout.main')
 @section('content')
+    <h1 class="mt-3 mb-3 text-center text-secondary">
+        <b> Our Products </b>
+    </h1>
 
-<h1 class="mt-3 mb-3 text-center text-secondary">
-    <b> Our Products </b>
-</h1>
-
-<div class="row justify-content-center">
-    <div class="col-md-6 mb-3">
-        <form action="/product">
-            <div class="input-group mb-3">
-                <input type="text" class="form-control" placeholder="Cari Produk" name="search" value="{{ request('search') }}">
-                <button class="btn btn-dark" type="submit">Search</button>
-            </div>
-        </form>
+    <div class="row justify-content-center">
+        <div class="col-md-6 mb-3">
+            <form action="/product">
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" placeholder="Cari Produk" name="search"
+                        value="{{ request('search') }}">
+                    <button class="btn btn-dark" type="submit">Search</button>
+                </div>
+            </form>
+        </div>
     </div>
-</div>
 
-@if ($product->count())
-<div class="container">
-    <div class="row">
-        @foreach ($product as $p)
-        <div class="col-md-4 mb-3">
-            <div class="card">
-                <div style="max-height: 500px; overflow:hidden">
-                    <img src="{{ asset('storage/' . $p->file_pendukung) }}" class="card-img-top" alt="{{ $p->file_pendukung }}">
-                </div>
-                <div class="card-body">
-                    <h5 class="card-title">{{ $p->nama }}</h5>
-                    <!-- <p class="card-text">{{ $p->deskripsi }}</p> -->
-                    <p class="card-text"><small class="text-muted">Last updated {{ $p->created_at->diffForHumans() }}</small></p>
-                    <form>
-                        <div class="value-button" id="decrease" onclick="decreaseValue()" value="Decrease Value">-</div>
-                        <input type="number" id="number" value="0" />
-                        <div class="value-button" id="increase" onclick="increaseValue()" value="Increase Value">+</div>
-                    </form>
-                    <button type="submit" class="btn btn-primary">Add to cart</button>
-                    <a href="/product/{{ $p->id }}" class="btn btn-secondary float-right">Read more</a>
-                </div>
+    @if ($product->count())
+        <div class="container">
+            <div class="row">
+                @foreach ($product as $p)
+                    <div class="col-md-4 mb-3">
+                        <div class="card">
+                            <div style="max-height: 500px; overflow:hidden">
+                                <img src="{{ asset('storage/' . $p->file_pendukung) }}" class="card-img-top"
+                                    alt="{{ $p->file_pendukung }}">
+                            </div>
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $p->nama }}</h5>
+                                <!-- <p class="card-text">{{ $p->deskripsi }}</p> -->
+                                <p class="card-text"><small class="text-muted">Last updated
+                                        {{ $p->created_at->diffForHumans() }}</small></p>
+                                <form action="/keranjang" method="POST">
+                                    @csrf
+                                    <div class="value-button" id="decrease" onclick="decreaseValue()"
+                                        value="Decrease Value">-</div>
+                                    <input type="number" name="jumlah" id="jumlah" value="0" />
+                                    <div class="value-button" id="increase" onclick="increaseValue()"
+                                        value="Increase Value">+</div>
+                                    <br>
+                                    <input type="hidden" name="user_id" value="{{ auth()->user()->id }}">
+                                    <input type="hidden" name="product_id" value="{{ $p->id }}">
+                                    <button type="submit" class="btn btn-primary btn-sm ">Add to cart</button>
+                                    <a href="/product/{{ $p->id }}"
+                                        class="btn btn-secondary float-right d-inline btn-sm">Read
+                                        more</a>
+                                </form>
+
+
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
             </div>
         </div>
-        @endforeach
-    </div>
-</div>
-@else
-<p class="text-dark text-center fs-4">Produk masih belum tersedia.</p>
-@endif
-
-
+    @else
+        <p class="text-dark text-center fs-4">Produk masih belum tersedia.</p>
+    @endif
 @endsection
