144e96fc71d9b21ad0e9f42554dc0f1d61f1a288
tzelginianc <tzelginia78@gmail.com>
2022-06-26 20:09:50 +0700

    Menambahkan relasi transaksi

--- app/Models/User.php
+++ app/Models/User.php
@@ -53,4 +53,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Keranjang::class);
     }
+
+    public function transaksi()
+    {
+        return $this->hasMany(Transaksi::class);
+    }
 }
