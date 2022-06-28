2a5557e7d1956173b3fdc6ca1541ab535a80f42b
= <=>
2022-06-28 14:14:36 +0700

    aboutUS

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,6 +2,31 @@
    @section('content')
    <!-- Page Content -->
    <!-- About Page Starts Here -->
-   <p> <mark> TOKO Pixie FURNITURE</mark></p>
+   <body>
+      <center><h1><font color="	#FFFAF0">Toko Pixie Furniture</font><br></h1> </center>
+   </body>
+
+
+
+   
+   <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+      <p>
+        Toko Pixie Furniture adalah toko yang menyediakan barang 
+        . Kita juga bisa menampilkan isi variabel
+         <var>name</var>,
+        seperti ini:
+    
+        <pre>
+          1. Meja
+          2. Kursi
+          3. Almari
+          4. Rak
+        </pre>
+    
+    </body>
    <!-- About Page Ends Here -->
    @endsection
