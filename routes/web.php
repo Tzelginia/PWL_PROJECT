1a76c67420e864ac48051ce885ae689a6a9d2b45
tzelginianc <tzelginia78@gmail.com>
2022-12-15 17:29:58 +0700

    tambah route

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\KeranjangController;
 use App\Http\Controllers\TransaksiController;
 use App\Http\Controllers\CetakController;
+use App\Illuminate\Support\Facades\Artisan;
 
 /*
 |--------------------------------------------------------------------------
